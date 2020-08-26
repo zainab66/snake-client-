559b00f99d9ed8ca11f144a3746f5277e6862305
zainab66 <zainab66r@yahoo.com>
2020-08-26 01:45:50 +0000

    play.js

--- play.js
+++ play.js
@@ -1,25 +1,4 @@
-const net = require('net');
 
-/**
- * Establishes connection with the game server
- */
-const connect = function() {
-  const conn = net.createConnection({ 
-    host: 'localhost',
-    port: 50541
-  });
-  // interpret incoming data as text
-  conn.setEncoding('utf8'); 
-  conn.on('data', (data) => {
-    console.log('Server says: ', data);
-  });
-  return conn;
-}
-
+const { connect } = require('./client');
 console.log('Connecting ...');
 connect();
-
-
-
-
-
