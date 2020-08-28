276d16e8d0ada754cb10fc79d6fd9e11558dbc62
zainab66 <zainab66r@yahoo.com>
2020-08-28 04:30:48 +0000

    Stdin Setup

--- play.js
+++ play.js
@@ -1,6 +1,28 @@
 
+//const connect  = require('./input');
 const connect  = require('./client');
 console.log('Connecting ...');
-console.log('zzzzzz ...');
-
 connect();
+
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data', key => {
+    handleUserInput(key);
+  });
+  return stdin;
+};
+
+const handleUserInput = (key) => {
+  const stdout = process.stdout;
+  if (key === '\u0003') {
+    stdout.write("Exited snek game. Bye bye.\n");
+    process.exit();
+  }
+};
+
+setupInput();
+handleUserInput(); 
